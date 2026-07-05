ab036b5c60eb429a205a71d954bffb6ed01f57aa
Lightning-Ari <arindamdas1116@outlook.com>
2026-07-05 15:16:23 +0000

    5th july 2026

---
+++ Python/object_oriented_programming/class/python-Book/main.py
@@ -0,0 +1,24 @@
+class Restaurant:
+    def __init__(self, name: str, cuisine_type: str) -> None:
+        self.restaurant_name: str = name
+        self.cusine_type: str = cuisine_type
+
+    def describe_restaurant(self) -> None:
+        print(f"The {self.restaurant_name} is {self.cusine_type} restaurant. Who love to share meal with others and known for good test.")
+
+    def open_restaurant(self) -> None:
+        print(f"The {self.restaurant_name} is open")
+
+
+
+my_restaurant = Restaurant("Golden Wings", "Chiness")
+
+my_restaurant.describe_restaurant()
+my_restaurant.open_restaurant()
+
+
+restaurant1 = Restaurant("Crab me!", "Many countries")
+restaurant2 = Restaurant("Wow! Momo", "MOMO")
+
+restaurant1.describe_restaurant()
+restaurant2.describe_restaurant()
